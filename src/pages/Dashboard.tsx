11a9f8ca5fa521d7a0cd4631f903350de3af6b37
Aufa Mutia <aufamutia5135@gmail.com>
2023-08-01 08:31:47 +0700

    dashboard belum fix

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,21 @@
+import { BackgroundImage } from "@mantine/core";
+import Bg from "/assets/img/background-biru.png";
+
 function Dashboard() {
-  return <></>;
+  return (
+    <div
+      style={{
+        position: "absolute",
+        width: "75%",
+        height: "100%",
+      }}
+    >
+      <BackgroundImage
+        src={Bg}
+        style={{ width: "100%", height: "100%", backgroundSize: "cover" }}
+      ></BackgroundImage>
+    </div>
+  );
 }
 
 export default Dashboard;
